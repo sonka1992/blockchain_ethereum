da2b2fc44b7187065560dd0c83ddd15181b8635b
sonka1992 <sonka1992@hotmail.com>
2018-05-06 20:28:47 +0200

    Init homework methods

---
+++ main.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.21;
+
+contract ReserveVS {
+
+    // <contract_variables>
+
+    // </contract_variables>
+
+    function ReserveVS(uint256 pricePerSlot) public {
+        // TODO
+    }
+
+    function reserve(uint24 slotId) public payable {
+        // TODO
+    }
+
+    function transfer(uint24 slotId, address to) public {
+        // TODO
+    }
+
+    function validate(address client) public view returns (bool) {
+        // TODO
+    }
+
+    function withdraw() public {
+        // TODO
+    }
+
+}
